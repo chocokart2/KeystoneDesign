d105cc212b88326c1e9b8646f0c7f849ae0c0395
이현서 <81573359+Leehyunseo12@users.noreply.github.com>
2024-05-23 09:16:33 +0900

    에너미 업데이트.cs
    에너미가 플레이어와의 거리가 일정 범위 안에 있으면 추적 속도로 플레이어를 향해 이동하고, 범위를 벗어나면 기본 속도로 이동하게 수정한 코드입니다.

--- Prototype 4 (Development)/Assets/Scripts/Enemy.cs
+++ Prototype 4 (Development)/Assets/Scripts/Enemy.cs
@@ -1,10 +1,11 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public float speed = 3.0f;
+    public float chaseSpeed = 5.0f; // 추적 속도
+    public float chaseRange = 10.0f; // 추적 범위
     public float pushForce = 55.0f; // 밀어내는 힘
 
     private Rigidbody enemyRb;
@@ -24,11 +25,24 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-        enemyRb.AddForce(lookDirection * speed);
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        // 플레이어와의 거리가 일정 범위 안에 있는 경우 추적 속도로 이동
+        if (distanceToPlayer <= chaseRange)
+        {
+            Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+            enemyRb.AddForce(lookDirection * chaseSpeed);
+        }
+        else
+        {
+            // 플레이어를 향해 이동 (기본 속도)
+            Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+            enemyRb.AddForce(lookDirection * speed);
+        }
+
+        // 플레이어의 버프 상태에 따른 밀어내기 로직
         if (playerController.isBuffActive)
         {
-            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
             if (distanceToPlayer < playerController.buff.safeDistance)
             {
                 // 플레이어로부터 밀어내는 방향 벡터 계산
@@ -36,18 +50,6 @@ public class Enemy : MonoBehaviour
                 // 적을 밀어내는 힘을 추가
                 enemyRb.AddForce(directionAwayFromPlayer * pushForce, ForceMode.Impulse);
             }
-            else
-            {
-                // 플레이어를 향해 이동
-                Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-                enemyRb.AddForce(lookDirection * speed);
-            }
-        }
-        else
-        {
-            // 플레이어를 향해 이동
-            Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-            enemyRb.AddForce(lookDirection * speed);
         }
 
         // 적이 너무 아래로 떨어지면 파괴
